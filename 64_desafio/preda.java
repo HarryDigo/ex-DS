bdffa03d9c12f6d0bfb6edde81c719621b7fb34a
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2024-03-12 15:27:49 -0300

    pedra papel tesoura feito

---
+++ 64_desafio/preda.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class preda {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random gen = new Random();
+        System.out.print ("Pedra(0), Papel(1) ou Tesoura(2)");
+        int n = input.nextInt();
+        int n2 = gen.nextInt() % 3;
+        switch(n) {
+            case 0:
+                if (n == n2) System.out.print("Empate!");
+                else if (n2 == 1) System.out.print("Você perdeu");
+                else System.out.print("Você venceu");
+                break;
+            case 1:
+                if (n == n2) System.out.print("Empate!");
+                else if (n2 == 2) System.out.print("Você perdeu");
+                else System.out.print("Você venceu");
+                break;
+            case 2:
+                if (n == n2) System.out.print("Empate!");
+                else if (n2 == 0) System.out.print("Você perdeu");
+                else System.out.print("Você venceu");
+            break;
+            default:
+                System.out.print ("número inválido");
+                break;
+        }
+        input.close();
+    }
+}
