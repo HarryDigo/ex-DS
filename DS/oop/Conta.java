b6aa5fd841d9b6d29a8f9f1fd7e52343a1e22849
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2024-05-07 15:29:10 -0300

    Mais uma aula de oop, conta modificada.
    duas adições para a conta, sendo o tipo e o limite.

--- DS/oop/Conta.java
+++ DS/oop/Conta.java
@@ -2,11 +2,14 @@ import javax.swing.JOptionPane;
 
 public class Conta {
     private String cliente;
+    private String tipo;
     private double saldo;
+    private double limite;
 
-    public Conta(String cliente, double saldo) {
+    public Conta(String cliente, double saldo, double limite) {
         this.cliente = cliente;
         this.saldo = saldo;
+        setLimite(limite);
     }
 
     public String getCliente() {
@@ -25,13 +28,55 @@ public class Conta {
         this.saldo = saldo;
     }
 
-    public void transacao(String tipo, double valor) {
-        if (tipo == "saque") {
-            valor = -valor;
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        if (limite > 0) limite = -limite;
+
+        if (limite > saldo) {
+            JOptionPane.showMessageDialog(null, "Seu saldo está abaixo do novo limite, tente novamente");
+            return;
         }
 
-        if (saldo + valor < 0) {
-            JOptionPane.showMessageDialog(null, cliente+"\n\nSeu saldo é insuficiente para esta transação");
+        this.limite = limite;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public boolean setTipo(int tipo) {
+        switch (tipo) {
+            case 1:
+                this.tipo = "corrente";
+                return true;
+
+            case 2:
+                this.tipo = "poupanca";
+                return true;
+        
+            default:
+                JOptionPane.showMessageDialog(null, "Tipo de conta inválido, tente novamente");
+                return false;
+        }
+    }
+
+    public void transacao(String tipo, double valor) {
+        if (valor < 0) {
+            JOptionPane.showMessageDialog(null, "Valor inválido, tente novamente;");
+            return;
+        }
+
+        if (tipo == "saque") {
+            valor = -valor;
+        } else if (this.tipo == "poupanca") {
+            valor *= 1.0005;
+        }
+
+        if (saldo + valor < limite) {
+            JOptionPane.showMessageDialog(null, cliente+"\nConta "+tipo+"\n\nSeu saldo é insuficiente para esta transação");
             return;
         }
 
--- DS/oop/ContaTest.java
+++ DS/oop/ContaTest.java
@@ -6,6 +6,7 @@ public class ContaTest {
         String aux;
         double valor;
         int op;
+        int tipo;
 
         do {
             aux = JOptionPane.showInputDialog(nome+"\n\nInsira o saldo inicial");
@@ -13,11 +14,25 @@ public class ContaTest {
 
         double saldo = Double.parseDouble(aux);
 
-        Conta conta1 = new Conta(nome, saldo);
+        do {
+            aux = JOptionPane.showInputDialog(nome+"\n\nInsira o limite da conta");
+        } while(!MyDouble.tryParse(aux));
+
+        double limite = Double.parseDouble(aux);
+
+        Conta conta1 = new Conta(nome, saldo, limite);
+
+        do {
+            do {
+                aux = JOptionPane.showInputDialog(conta1.getCliente()+"\n\nSelecione o tipo de conta:\n1 - corrente\n2 - poupança");
+            } while(!MyInt.tryParse(aux));
+            
+            tipo = Integer.parseInt(aux);
+        } while (!conta1.setTipo(tipo));
 
         while (true) {
             do {
-                aux = JOptionPane.showInputDialog(conta1.getCliente()+"\n\nSelecione a operação:\n1 - depósito\n2 - saque\n3 - exibir saldo\n4 - mudar nome\n0 - sair");
+                aux = JOptionPane.showInputDialog(conta1.getCliente()+"\nConta "+conta1.getTipo()+"\n\nSelecione a operação:\n1 - depósito\n2 - saque\n3 - exibir saldo\n4 - mudar nome\n5 - exibir limite\n6 - mudar limite\n0 - sair");
             } while(!MyInt.tryParse(aux));
 
             op = Integer.parseInt(aux);
@@ -27,7 +42,7 @@ public class ContaTest {
 
                 case 1:
                     do {
-                        aux = JOptionPane.showInputDialog(conta1.getCliente()+"\n\nInsira o valor do depósito");
+                        aux = JOptionPane.showInputDialog(conta1.getCliente()+"\nConta "+conta1.getTipo()+"\n\nInsira o valor do depósito");
                     } while (!MyDouble.tryParse(aux));
 
                     valor = Double.parseDouble(aux);
@@ -37,7 +52,7 @@ public class ContaTest {
                 
                 case 2:
                     do {
-                        aux = JOptionPane.showInputDialog(conta1.getCliente()+"\n\nInsira o valor do saque");
+                        aux = JOptionPane.showInputDialog(conta1.getCliente()+"\nConta "+conta1.getTipo()+"\n\nInsira o valor do saque");
                     } while (!MyDouble.tryParse(aux));
 
                     valor = Double.parseDouble(aux);
@@ -46,16 +61,30 @@ public class ContaTest {
                     break;
 
                 case 3:
-                    JOptionPane.showMessageDialog(null, String.format("%s\n\nSeu saldo atual é: R$%.2f", conta1.getCliente(), conta1.getSaldo()));
+                    JOptionPane.showMessageDialog(null, String.format("%s\nConta %s\n\nSeu saldo atual é: R$%.2f", conta1.getCliente(), conta1.getTipo(), conta1.getSaldo()));
                     break;
 
                 case 4:
-                    nome = JOptionPane.showInputDialog(conta1.getCliente()+"\n\nInsira o novo nome da conta");
+                    nome = JOptionPane.showInputDialog(conta1.getCliente()+"\nConta "+conta1.getTipo()+"\n\nInsira o novo nome da conta");
                     conta1.setCliente(nome);
+                    break;
+                    
+                case 5:
+                    JOptionPane.showMessageDialog(null, String.format("%s\nConta %s\n\nSeu limite atual é: R$%.2f", conta1.getCliente(), conta1.getTipo(), conta1.getLimite()));
+                    break;
+
+                case 6:
+                    do {
+                        aux = JOptionPane.showInputDialog(String.format("%s\nConta %s\n\nSeu limite atual é: R$%.2f\n\nInsira o novo limite da conta", conta1.getCliente(), conta1.getTipo(), conta1.getLimite()));
+                    } while (!MyDouble.tryParse(aux));
+
+                    limite = Double.parseDouble(aux);
+                    
+                    conta1.setLimite(limite);
                     break;
 
                 default:
-                    JOptionPane.showMessageDialog(null, conta1.getCliente()+"\n\nOperação inválida, tente novamente");
+                    JOptionPane.showMessageDialog(null, conta1.getCliente()+"\nConta "+conta1.getTipo()+"\n\nOperação inválida, tente novamente");
                     break;
             }
         }
