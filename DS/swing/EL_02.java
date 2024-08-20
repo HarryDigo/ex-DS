7b33e002129e99a94776d2f7f1d52550b00e94af
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2024-08-20 15:56:21 -0300

    hole mamole claucladora

--- DS/swing/EL_02.java
+++ DS/swing/EL_02.java
@@ -1,10 +1,17 @@
-import  java.awt.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.*;
+
 
 public class EL_02 extends JFrame {
     private final JTextField txtCalc;
     private final JButton btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDot, btnEqual, btnSum, btnSub, btnMult, btnDiv;
     private final JPanel panel1, panel2;
+
+    private String operation = "";
+    private ArrayList<String> elements;
+    private ArrayList<String> result;
 
     public EL_02 () {
         super("Calculadora");
@@ -27,6 +34,89 @@ public class EL_02 extends JFrame {
         btnSub = new JButton("-");
         btnMult = new JButton("*");
         btnDiv = new JButton("/");
+
+        txtCalc.setEditable(false);
+
+        btnEqual.addActionListener((actionEvent) -> {
+            elements = new ArrayList<>(Arrays.asList(operation.split("\\s+")));
+            result = evaluate(elements);
+            System.out.println(result.toString());
+        });
+
+        btnSum.addActionListener((actionEvent) -> {
+            operation += " + ";
+            txtCalc.setText(operation);
+        });
+
+        btnSub.addActionListener((actionEvent) -> {
+            operation += " - ";
+            txtCalc.setText(operation);
+        });
+
+        btnMult.addActionListener((actionEvent) -> {
+            operation += " * ";
+            txtCalc.setText(operation);
+        });
+
+        btnDiv.addActionListener((actionEvent) -> {
+            operation += " / ";
+            txtCalc.setText(operation);
+        });
+
+        btn0.addActionListener((actionEvent) -> {
+            operation += "0";
+            txtCalc.setText(operation);
+        });
+
+        btn1.addActionListener((actionEvent) -> {
+            operation += "1";
+            txtCalc.setText(operation);
+        });
+
+        btn2.addActionListener((actionEvent) -> {
+            operation += "2";
+            txtCalc.setText(operation);
+        });
+
+        btn3.addActionListener((actionEvent) -> {
+            operation += "3";
+            txtCalc.setText(operation);
+        });
+
+        btn4.addActionListener((actionEvent) -> {
+            operation += "4";
+            txtCalc.setText(operation);
+        });
+
+        btn5.addActionListener((actionEvent) -> {
+            operation += "5";
+            txtCalc.setText(operation);
+        });
+
+        btn6.addActionListener((actionEvent) -> {
+            operation += "6";
+            txtCalc.setText(operation);
+        });
+
+        btn7.addActionListener((actionEvent) -> {
+            operation += "7";
+            txtCalc.setText(operation);
+        });
+
+        btn8.addActionListener((actionEvent) -> {
+            operation += "8";
+            txtCalc.setText(operation);
+        });
+
+        btn9.addActionListener((actionEvent) -> {
+            operation += "9";
+            txtCalc.setText(operation);
+        });
+
+        btnDot.addActionListener((actionEvent) -> {
+            operation += ".";
+            txtCalc.setText(operation);
+        });
         
         panel1 = new JPanel();
         panel2 = new JPanel();
@@ -63,8 +153,93 @@ public class EL_02 extends JFrame {
         pack();
     }
 
+    private static ArrayList<String> evaluate(ArrayList<String> elements) {
+        System.out.println(elements.toString());
+        double x, y;
+        int i = 0;
+
+        if(elements.contains("*") || elements.contains("/")) {
+            while(true) {
+                if(elements.contains("*") || elements.contains("/")) {
+                    if (elements.get(i).equals("*")) {
+                        x = MyDouble.tryParse(elements.get(i - 1));
+                        y = MyDouble.tryParse(elements.get(i + 1));
+
+                        elements.set(i - 1, Double.toString(x * y));
+                        elements.remove(i + 1);
+                        elements.remove(i);
+
+                        break;
+                    }
+
+                    if (elements.get(i).equals("/")) {
+                        x = MyDouble.tryParse(elements.get(i - 1));
+                        y = MyDouble.tryParse(elements.get(i + 1));
+
+                        elements.set(i - 1, Double.toString(x / y));
+                        elements.remove(i + 1);
+                        elements.remove(i);
+
+                        break;
+                    }
+
+                    if(i < elements.size() - 1) i++;
+                    else i = 0;
+                }
+                else break;
+            }
+        }
+
+        i = 0;
+
+        if(elements.contains("+") || elements.contains("-")) {
+            while(true) {
+                if(elements.contains("+") || elements.contains("-")) {
+                    if (elements.get(i).equals("+")) {
+                        x = MyDouble.tryParse(elements.get(i - 1));
+                        y = MyDouble.tryParse(elements.get(i + 1));
+
+                        elements.set(i - 1, Double.toString(x + y));
+                        elements.remove(i + 1);
+                        elements.remove(i);
+
+                        break;
+                    }
+
+                    if (elements.get(i).equals("-")) {
+                        x = MyDouble.tryParse(elements.get(i - 1));
+                        y = MyDouble.tryParse(elements.get(i + 1));
+
+                        elements.set(i - 1, Double.toString(x - y));
+                        elements.remove(i + 1);
+                        elements.remove(i);
+
+                        break;
+                    }
+
+                    if(i < elements.size() - 1) i++;
+                    else i = 0;
+                }
+                else break;
+            }
+        }
+
+        if (elements.size() == 1) return elements;
+        else return evaluate(elements);
+    }
+
     public static void main(String[] args) {
         EL_02 ex4 = new EL_02();
         ex4.setVisible(true);
+    }
+}
+
+class MyDouble {
+    public static double tryParse(String value) {
+        try {
+            return Double.parseDouble(value);
+        } catch(NumberFormatException e) {
+            return 0;
+        }
     }
 }
