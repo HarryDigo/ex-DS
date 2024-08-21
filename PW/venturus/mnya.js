e24dd03bb978c53586159cb5e951d4e6f79de187
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2024-08-21 12:00:09 -0300

    venturus

---
+++ PW/venturus/mnya.js
@@ -0,0 +1,17 @@
+console.log("não sabia que se voce só rodava ele o codigo ia");
+
+console.log("outro útil é o promt(), faz uma prompt")
+
+console.log("outra coisa é o objeto, ahco que esqueci como faz");
+
+const bola = {
+    uwu: true,
+    furry: "sim",
+    owo: 727
+}
+
+console.log("foreach(objeto, index) retorna cada item e seu index de um array")
+
+array.forEach((element, i) => {
+    console.log()
+});
